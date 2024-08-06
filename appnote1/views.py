1673336da4991ff77a536f83d3cb5397f66b7160
vincent <vincentayuba69@gmail.com>
2024-08-06 12:31:24 +0100

    before adding bootstrap

--- appnote1/views.py
+++ appnote1/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+	return render(request,'appnote1/dashboard.html')
+
+def customer(request):
+	return render(request,'appnote1/customer.html')
+
+def products(request):
+	return render(request,'appnote1/products.html')
+
